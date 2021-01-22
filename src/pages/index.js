a288cd17334a517c441f8563990f8a7e1dbb971a
Ryan <hello@ryanwu.me>
2021-01-22 14:23:03 -0500

    Added Tailwind CSS

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto px-4">
+      <div>
+      <section className="py-12 px-4 text-center">
+        <div className="w-full max-w-2xl mx-auto">
+          <span className="text-sm font-semibold">Next.js + Tailwind CSS + Firebase Cloud Functions + Github Actions</span>
+          < h2 className = "text-5xl mt-2 mb-6 leading-tight font-semibold font-heading" >Project Header</h2>
+          <a className="text-indigo-600 hover:underline" href="#">Learn more &raquo;</a>
+        </div>
+      </section>
+      </div>
+    </main>
+  )
+}
